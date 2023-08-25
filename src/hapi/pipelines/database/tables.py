44444537de0f9e053304acd57dc8db1a62724a64
turnerm <turnermon@gmail.com>
2023-08-25 11:45:21 -0400

    Add some doctrings

--- src/hapi/pipelines/database/tables.py
+++ src/hapi/pipelines/database/tables.py
@@ -1,7 +1,10 @@
+"""Reflect and return the tables from the HAPI DB."""
+
 from typing import Type
 
 from sqlalchemy.orm import Session, declarative_base
 
+# Each time a new theme is added, add the tables here
 _TABLE_NAMES = [
     "Admin1",
     "Admin2",
@@ -14,20 +17,29 @@ _TABLE_NAMES = [
     "OrgType",
     "Population",
     "Resource",
+    "Sector",
 ]
 
 
 class Tables:
     def __init__(self, session: Session):
+        """
+        Reflect the tables from an existing DB.
+
+        Args:
+            session ():
+        """
         engine = session.get_bind()
         Base = declarative_base()
         Base.metadata.reflect(engine)
 
-        self.create_table_classes(Base)
+        self._create_table_classes(Base)
 
     @staticmethod
-    def create_table_classes(Base: Type[declarative_base()]):
+    def _create_table_classes(Base: Type[declarative_base()]):
         for table_name in _TABLE_NAMES:
+            # Create each class dynamically with table_name as the name,
+            # Base as the base, and dunder table from the Base metadata
             table_class = type(
                 table_name,
                 (Base,),
