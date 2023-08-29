186f5af89a7bee23962d90370d506a46be37738c
turnerm <turnermon@gmail.com>
2023-08-29 14:47:53 -0400

    simplifly import

--- src/hapi/pipelines/database/base.py
+++ src/hapi/pipelines/database/base.py
@@ -1,5 +1,3 @@
 """Base file so that all tables are connected to the same DB Base."""
 
-from hdx.database.no_timezone import Base as BaseNoTZ
-
-Base = BaseNoTZ
+from hdx.database.no_timezone import Base  # noqa
