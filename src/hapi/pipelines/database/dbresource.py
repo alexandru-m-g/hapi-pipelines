d5a6ae6c93365fdeabd0ae39953a8cf2be79db77
Mike <rans@un.org>
2023-08-31 11:18:45 +1200

    try to fix merge issues

---
+++ src/hapi/pipelines/database/dbresource.py
@@ -0,0 +1,28 @@
+"""Resource table."""
+
+from hdx.database.no_timezone import Base
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+)
+from sqlalchemy.orm import relationship
+
+
+class DBResource(Base):
+    __tablename__ = "Resource"
+
+    id = Column(Integer, primary_key=True)
+    dataset_ref = Column(ForeignKey("Dataset.id"), nullable=False)
+    hdx_link = Column(String(512), nullable=False)
+    code = Column(String(128), nullable=False)
+    filename = Column(String(256), nullable=False)
+    format = Column(String(32), nullable=False)
+    update_date = Column(DateTime, nullable=False, index=True)
+    is_hxl = Column(Boolean, nullable=False, index=True)
+    api_link = Column(String(1024), nullable=False)
+
+    Dataset = relationship("DBDataset")
