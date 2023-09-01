dcaa549702d755330d0221c731a04d551ce6bc36
Mike <rans@un.org>
2023-09-01 13:25:17 +1200

    Tables in SQLAlchemy ORM format

--- src/hapi/pipelines/database/dbdataset.py
+++ src/hapi/pipelines/database/dbdataset.py
@@ -1,16 +1,21 @@
 """Dataset table."""
 
 from hdx.database.no_timezone import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class DBDataset(Base):
     __tablename__ = "dataset"
 
-    id = Column(Integer, primary_key=True)
-    hdx_link = Column(String(512), nullable=False)
-    code = Column(String(128), nullable=False)
-    title = Column(String(1024), nullable=False)
-    provider_code = Column(String(128), nullable=False, index=True)
-    provider_name = Column(String(512), nullable=False, index=True)
-    api_link = Column(String(1024), nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    hdx_link: Mapped[str] = mapped_column(String(512), nullable=False)
+    code: Mapped[str] = mapped_column(String(128), nullable=False)
+    title = mapped_column(String(1024), nullable=False)
+    provider_code: Mapped[str] = mapped_column(
+        String(128), nullable=False, index=True
+    )
+    provider_name: Mapped[str] = mapped_column(
+        String(512), nullable=False, index=True
+    )
+    api_link: Mapped[str] = mapped_column(String(1024), nullable=False)
