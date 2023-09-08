781db8b9baba1d6ef37980ad6e34403bcc7e7627
turnerm <turnermon@gmail.com>
2023-09-08 12:35:43 -0400

    Fixing connector codes

--- src/hapi/pipelines/utilities/admins.py
+++ src/hapi/pipelines/utilities/admins.py
@@ -1,7 +1,6 @@
 import logging
 from abc import ABC
-from datetime import datetime
-from typing import Dict, List
+from typing import Dict, List, Literal
 
 import hxl
 from hdx.utilities.dateparse import parse_date
@@ -12,9 +11,12 @@ from sqlalchemy.orm import Session
 
 from hapi.pipelines.database.db_admin1 import DBAdmin1
 from hapi.pipelines.database.db_admin2 import DBAdmin2
+from hapi.pipelines.database.db_location import DBLocation
 from hapi.pipelines.utilities.locations import Locations
 
 logger = logging.getLogger(__name__)
+
+_ADMIN_LEVELS = ("1", "2")
 
 
 class Admins:
@@ -36,21 +38,36 @@ class Admins:
             logger.exception("Download of admin info from dataset failed!")
             return
 
-        self._update_admin_table(desired_adminlevel="1", admin_info=admin_info)
+        self._update_admin_table(
+            desired_admin_level="1",
+            admin_info=admin_info,
+            parent_dict=self.locations.data,
+        )
+        self._add_admin1_connector_rows()
         results = self.session.execute(select(DBAdmin1.id, DBAdmin1.code))
         self.data = {result[1]: result[0] for result in results}
-        self._update_admin_table(desired_adminlevel="2", admin_info=admin_info)
+        self._update_admin_table(
+            desired_admin_level="2",
+            admin_info=admin_info,
+            parent_dict=self.data,
+        )
+        self._add_admin2_connector_rows()
         results = self.session.execute(select(DBAdmin2.id, DBAdmin2.code))
         for result in results:
             self.data[result[1]] = result[0]
 
     def _update_admin_table(
-        self, desired_adminlevel: str, admin_info: Dataset
+        self,
+        desired_admin_level: Literal[_ADMIN_LEVELS],
+        admin_info: Dataset,
+        parent_dict: Dict,
     ):
-        parent_dict = {"1": self.locations.data, "2": self.data}
-        admin_filter = AdminFilter(
+        if desired_admin_level not in _ADMIN_LEVELS:
+            raise ValueError(f"Admin levels must be one of {_ADMIN_LEVELS}")
+        # Filter admin level and countries
+        admin_filter = _AdminFilter(
             source=admin_info,
-            desired_admin_level=desired_adminlevel,
+            desired_admin_level=desired_admin_level,
             country_codes=list(self.locations.data.keys()),
         )
         for i, row in enumerate(admin_filter):
@@ -59,26 +76,25 @@ class Admins:
             name = row.get("#adm+name")
             reference_period_start = parse_date(row.get("#date+start"))
             parent = row.get("#adm+code+parent")
-            parent_ref = parent_dict[desired_adminlevel].get(parent)
+            parent_ref = parent_dict.get(parent)
             # Catch edge cases
             if not parent_ref:
                 # TODO: should this be in the config somehow?
                 # Abyei
                 if code == "SS0001":
-                    # TODO: change this to blank ref
                     logger.warning(f"Changing {code}")
                     parent_ref = self.data["SSD-XXX"]
                 else:
                     logger.warning(f"Missing parent {parent} for code {code}")
                     continue
-            if desired_adminlevel == "1":
+            if desired_admin_level == "1":
                 admin_row = DBAdmin1(
                     location_ref=parent_ref,
                     code=code,
                     name=name,
                     reference_period_start=reference_period_start,
                 )
-            else:
+            elif desired_admin_level == "2":
                 admin_row = DBAdmin2(
                     admin1_ref=parent_ref,
                     code=code,
@@ -89,35 +105,73 @@ class Admins:
             if i % self.limit == 0:
                 self.session.commit()
         self.session.commit()
-        # Create null relation rows with parents
-        for parent_code, parent_ref in parent_dict[desired_adminlevel].items():
-            if desired_adminlevel == "1":
-                admin_row = DBAdmin1(
-                    location_ref=parent_ref,
-                    code=f"{parent_code}-XXX",
-                    name="UNSPECIFIED",
-                    is_unspecified=True,
-                    # TODO: should this be made nullable?
-                    #  Putting dummy data for now
-                    reference_period_start=datetime(2000, 1, 1),
-                )
-            else:
-                admin_row = DBAdmin2(
-                    admin1_ref=parent_ref,
-                    code=f"{parent_code}-XXX-XXX",
-                    name="UNSPECIFIED",
-                    is_unspecified=True,
-                    reference_period_start=datetime(2000, 1, 1),
-                )
+
+    def _add_admin1_connector_rows(self):
+        for location_code, location_ref in self.locations.data.items():
+            reference_period_start = (
+                self.session.query(DBLocation)
+                .filter(DBLocation.id == location_ref)
+                .one()
+                .reference_period_start
+            )
+            admin_row = DBAdmin1(
+                location_ref=location_ref,
+                code=get_admin1_to_location_connector_code(
+                    location_code=location_code
+                ),
+                name="UNSPECIFIED",
+                is_unspecified=True,
+                reference_period_start=reference_period_start,
+            )
+            self.session.add(admin_row)
+        self.session.commit()
+
+    def _add_admin2_connector_rows(self):
+        for admin1_code, admin1_ref in self.data.items():
+            reference_period_start = (
+                self.session.query(DBAdmin1)
+                .filter(DBAdmin1.id == admin1_ref)
+                .one()
+                .reference_period_start
+            )
+            admin_row = DBAdmin2(
+                admin1_ref=admin1_ref,
+                code=get_admin2_to_admin1_connector_code(
+                    admin1_code=admin1_code
+                ),
+                name="UNSPECIFIED",
+                is_unspecified=True,
+                reference_period_start=reference_period_start,
+            )
             self.session.add(admin_row)
         self.session.commit()
 
 
-class AdminFilter(AbstractStreamingFilter, ABC):
+def get_admin2_to_admin1_connector_code(admin1_code: str) -> str:
+    """Get the code for an unspecified admin2, based on the admin1 code.
+
+    Note that if you need to make the connection between admin2 and
+    location, and only know the location code, you'll need to pass the
+    output of get_admin1_to_location_connector_code to this function, e.g.
+    ```
+    location_code = "ABC"
+    admin1_code = get_admin1_to_location_connector_code(location_code)
+    admin2_code = get_admin2_to_admin1_connector_code(admin1_code)
+    ```
+    """
+    return f"{admin1_code}-XXX-XXX"
+
+
+def get_admin1_to_location_connector_code(location_code: str) -> str:
+    """Get the code for an unspecified admin1, based on the location code."""
+    return f"{location_code}-XXX"
+
+
+class _AdminFilter(AbstractStreamingFilter, ABC):
     def __init__(
         self,
         source: Dataset,
-        desired_admin_level: str,
+        desired_admin_level: Literal[_ADMIN_LEVELS],
         country_codes: List[str],
     ):
         self._desired_admin_level = desired_admin_level
