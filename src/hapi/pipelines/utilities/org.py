65ac3f5537b1a4e3187bfd58e00565f1ada0268a
Briar <briar.j.mills@gmail.com>
2023-09-22 11:18:53 -0600

    Correct parameter issues

--- src/hapi/pipelines/utilities/org.py
+++ src/hapi/pipelines/utilities/org.py
@@ -16,21 +16,24 @@ class Org:
     def populate_single(
         self,
         acronym,
-        org_name,
+        orgname,
         orgtype,
         reference_period_start,
         reference_period_end,
     ):
-        logger.info(f"Adding org {org_name}")
+        logger.info(f"Adding org {orgname}")
+        # TODO: add HDX link for orgs. What if there is no hdx link?
+        hdx_link = ""
         org_row = DBOrg(
+            hdx_link=hdx_link,
             acronym=acronym,
-            org_name=org_name,
-            orgtype=orgtype,
+            name=orgname,
+            org_type_code=orgtype,
             reference_period_start=reference_period_start,
             reference_period_end=reference_period_end,
         )
         self._session.add(org_row)
         self._session.commit()
-        results = self._session.execute(select(DBOrg.id, DBOrg.acronym, DBOrg.name, DBOrg.org_type))
+        results = self._session.execute(select(DBOrg.id, DBOrg.acronym, DBOrg.name, DBOrg.org_type_code))
         for result in results:
             self.data[(result[1], result[2], result[3])] = result[0]
