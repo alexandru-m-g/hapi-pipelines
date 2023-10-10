c13f4db13c3a58c142dfc5a3f2b43c2a0aefa7e9
turnerm <turnermon@gmail.com>
2023-10-10 16:57:12 +0200

    Update org type schema module name

--- src/hapi/pipelines/database/org_type.py
+++ src/hapi/pipelines/database/org_type.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Dict
 
-from hapi_schema.db_orgtype import DBOrgType
+from hapi_schema.db_org_type import DBOrgType
 from hdx.scraper.utilities.reader import Read
 from sqlalchemy.orm import Session
 
