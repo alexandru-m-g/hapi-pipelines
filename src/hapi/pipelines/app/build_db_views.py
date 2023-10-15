1847668efbb1d085c48ade40f3042712f2d2cae4
turnerm <turnermon@gmail.com>
2023-10-16 00:34:42 +0200

    HAPI-224 update view param var name

--- src/hapi/pipelines/app/build_db_views.py
+++ src/hapi/pipelines/app/build_db_views.py
@@ -1,31 +1,31 @@
-from hapi_schema.db_admin1 import admin1_view_params
-from hapi_schema.db_admin2 import admin2_view_params
-from hapi_schema.db_age_range import age_range_view_params
-from hapi_schema.db_dataset import dataset_view_params
-from hapi_schema.db_gender import gender_view_params
-from hapi_schema.db_location import location_view_params
+from hapi_schema.db_admin1 import view_params_admin1
+from hapi_schema.db_admin2 import view_params_admin2
+from hapi_schema.db_age_range import view_params_age_range
+from hapi_schema.db_dataset import view_params_dataset
+from hapi_schema.db_gender import view_params_gender
+from hapi_schema.db_location import view_params_location
 from hapi_schema.db_operational_presence import (
-    operational_presence_view_params,
+    view_params_operational_presence,
 )
-from hapi_schema.db_org import org_view_params
-from hapi_schema.db_org_type import org_type_view_params
-from hapi_schema.db_population import population_view_params
-from hapi_schema.db_resource import resource_view_params
-from hapi_schema.db_sector import sector_view_params
+from hapi_schema.db_org import view_params_org
+from hapi_schema.db_org_type import view_params_org_type
+from hapi_schema.db_population import view_params_population
+from hapi_schema.db_resource import view_params_resource
+from hapi_schema.db_sector import view_params_sector
 from hdx.database.views import view
 
 for view_params in [
-    admin1_view_params,
-    admin2_view_params,
-    age_range_view_params,
-    dataset_view_params,
-    gender_view_params,
-    location_view_params,
-    operational_presence_view_params,
-    org_view_params,
-    org_type_view_params,
-    population_view_params,
-    resource_view_params,
-    sector_view_params,
+    view_params_admin1,
+    view_params_admin2,
+    view_params_age_range,
+    view_params_dataset,
+    view_params_gender,
+    view_params_location,
+    view_params_operational_presence,
+    view_params_org,
+    view_params_org_type,
+    view_params_population,
+    view_params_resource,
+    view_params_sector,
 ]:
     view(**view_params.__dict__)
