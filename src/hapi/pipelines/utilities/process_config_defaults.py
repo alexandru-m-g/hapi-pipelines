d5154d7a1816fa7b0b50dad66580bafd45e5924a
SimonbJohnson <simonbjohnson@gmail.com>
2023-10-17 12:36:51 +0100

    using rsplit instead of split

--- src/hapi/pipelines/utilities/process_config_defaults.py
+++ src/hapi/pipelines/utilities/process_config_defaults.py
@@ -8,17 +8,17 @@ def combine_default(country, default):
 def find_defaults(config):
     default_list = []
     for key in config:
-        key_components = key.split("_")
-        if len(key_components) > 1 and key_components[1] == "default":
+        key_components = key.rsplit("_", 1)
+        if len(key_components) == 2 and key_components[1] == "default":
             default_list.append(key)
     return default_list
 
 
 def find_matching_top_level_keys(config, default_key):
     matching_top_level_keys = []
-    default_prefix = default_key.split("_")[0]
+    default_prefix = default_key.rsplit("_", 1)[0]
     for top_level_key in config:
-        top_level_key_prefix = top_level_key.split("_")[0]
+        top_level_key_prefix = top_level_key.rsplit("_", 1)[0]
         if (
             top_level_key != default_key
             and top_level_key_prefix == default_prefix
