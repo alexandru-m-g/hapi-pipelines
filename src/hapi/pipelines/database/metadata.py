2c974abd54ebcf626fa58a081e63106088dabab3
Mike <rans@un.org>
2023-10-25 14:42:14 +1300

    Rename filename -> name

--- src/hapi/pipelines/database/metadata.py
+++ src/hapi/pipelines/database/metadata.py
@@ -53,11 +53,10 @@ class Metadata(BaseUploader):
                     if sheet["is_hxlated"]:
                         is_hxlated = True
                         break
-                # TODO: Change resource "filename" to "name" in hdx-python-scraper (HAPI-246)
                 resource_row = DBResource(
                     hdx_id=resource_id,
                     dataset_ref=dataset_row.id,
-                    name=resource["filename"],
+                    name=resource["name"],
                     format=resource["format"],
                     update_date=resource["update_date"],
                     is_hxl=is_hxlated,
