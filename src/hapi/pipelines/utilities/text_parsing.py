f951eb442ec5c329e34f63e231ea61ce51c08f9a
Briar <briar.j.mills@gmail.com>
2023-10-26 23:31:57 -0400

    Add docstring, check for code

--- src/hapi/pipelines/utilities/text_parsing.py
+++ src/hapi/pipelines/utilities/text_parsing.py
@@ -6,22 +6,34 @@ from hdx.location.phonetics import Phonetics
 
 def get_code_from_name(
     name: str, code_lookup: Dict[str, str], code_mapping: Dict[str, str]
-) -> str:
+) -> str | None:
+    """
+    Given a name (org type, sector, etc), return the corresponding code.
+
+    Args:
+        name (str): Name to match
+        code_lookup (dict): Dictionary of official names and codes
+        code_mapping (dict): Additional dictionary of unofficial mappings provided by user
+
+    Returns:
+        str or None: matching code
+    """
     code = code_lookup.get(name)
     if code:
         return code
     name_clean = clean_name(name)
     code = code_mapping.get(name_clean)
-    if not code:
-        names = list(code_lookup.keys())
-        names_lower = [x.lower() for x in names]
-        name_index = Phonetics().match(
-            possible_names=names_lower,
-            name=name,
-            alternative_name=name_clean,
-        )
-        if name_index is None:
-            return None
-        name = names[name_index]
-        code = code_lookup.get(name)
+    if code:
+        return code
+    names = list(code_lookup.keys())
+    names_lower = [x.lower() for x in names]
+    name_index = Phonetics().match(
+        possible_names=names_lower,
+        name=name,
+        alternative_name=name_clean,
+    )
+    if name_index is None:
+        return None
+    name = names[name_index]
+    code = code_lookup.get(name)
     return code
