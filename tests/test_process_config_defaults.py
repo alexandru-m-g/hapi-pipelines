9229cf356aa9ff9edbc994e25672e635d557a4c8
Mike <rans@un.org>
2023-12-21 16:32:07 +1300

    Fix test

--- tests/test_process_config_defaults.py
+++ tests/test_process_config_defaults.py
@@ -1,3 +1,5 @@
+from os.path import join
+
 from hdx.utilities.loader import load_and_merge_yaml
 
 from hapi.pipelines.utilities.process_config_defaults import add_defaults
@@ -5,20 +7,20 @@ from hapi.pipelines.utilities.process_config_defaults import add_defaults
 
 def test_process_config_defaults():
     config_dict = load_and_merge_yaml(
-        ["tests/test_process_config_defaults.yaml"]
+        [join("tests", "test_process_config_defaults.yaml")]
     )
     config_dict = add_defaults(config_dict)
     expecdted_config_dict = {
         "theme_name_national": {
             "theme_name_foo": {
-                "other_parmas": "pass",
+                "other_params": "pass",
                 "input": ["c4", "c1", "c2", "c3"],
                 "list": ["c4", "c1", "c2", "c3"],
                 "output": ["c4", "c1", "c2", "c3"],
                 "output_hxl": ["#c4", "#c1", "#c2", "#c3"],
             },
             "theme_name_bar": {
-                "other_parmas": "pass",
+                "other_params": "pass",
                 "input": ["c1", "c2", "c3"],
                 "list": ["c1", "c2", "c3"],
                 "output": ["c1", "c2", "c3"],
