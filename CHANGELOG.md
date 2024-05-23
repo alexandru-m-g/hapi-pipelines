00dbfe167e592ecceafabdfd58e85407207a8741
Briar <briar.j.mills@gmail.com>
2024-05-23 14:33:53 -0400

    Update conflict pipeline and test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [0.9.7] - 2024-05-24
+
+### Changed
+
+- Filter miscoded unit in conflict data
+- Correct overwriting issue in conflict pipeline
+
 ## [0.9.6] - 2024-05-23
 
 ### Added
--- src/hapi/pipelines/database/conflict_event.py
+++ src/hapi/pipelines/database/conflict_event.py
@@ -34,70 +34,78 @@ class ConflictEvent(BaseUploader):
         errors = set()
         for dataset in self._results.values():
             dataset_name = dataset["hdx_stub"]
+            # TODO: once unit has been recoded remove filter and error
+            if dataset_name.startswith("colombia"):
+                add_message(
+                    errors,
+                    dataset_name,
+                    "filtering out unit El Tablon due to miscoding",
+                )
             rows = []
             number_duplicates = 0
             for admin_level, admin_results in dataset["results"].items():
+                # TODO: this is only one resource id, but three resources are downloaded per dataset
                 resource_id = admin_results["hapi_resource_metadata"]["hdx_id"]
-                resource_name = admin_results["hapi_resource_metadata"]["name"]
                 hxl_tags = admin_results["headers"][1]
                 admin_codes = list(admin_results["values"][0].keys())
                 values = admin_results["values"]
-
-                event_types = [et.value for et in EventType]
-                event_type = [et for et in event_types if et in resource_name][
-                    0
-                ]
 
                 for admin_code in admin_codes:
                     admin2_code = admins.get_admin2_code_based_on_level(
                         admin_code=admin_code, admin_level=admin_level
                     )
                     for irow in range(len(values[0][admin_code])):
-                        events = values[hxl_tags.index("#event+num")][
-                            admin_code
-                        ][irow]
-                        fatalities = None
-                        if "#fatalities+num" in hxl_tags:
-                            fatalities = values[
-                                hxl_tags.index("#fatalities+num")
+                        for et in EventType:
+                            event_type = et.value
+                            events = values[
+                                hxl_tags.index(f"#event+num+{event_type}")
                             ][admin_code][irow]
-                        month = values[hxl_tags.index("#date+month")][
-                            admin_code
-                        ][irow]
-                        year = values[hxl_tags.index("#date+year")][
-                            admin_code
-                        ][irow]
-                        time_period_range = parse_date_range(
-                            f"{month} {year}", "%B %Y"
-                        )
-                        row = (
-                            resource_id,
-                            admin2_code,
-                            event_type,
-                            events,
-                            fatalities,
-                            time_period_range[0],
-                            time_period_range[1],
-                        )
-                        if row in rows:
-                            number_duplicates += 1
-                            continue
-                        rows.append(row)
-                        conflict_event_row = DBConflictEvent(
-                            resource_hdx_id=resource_id,
-                            admin2_ref=self._admins.admin2_data[admin2_code],
-                            event_type=event_type,
-                            events=events,
-                            fatalities=fatalities,
-                            reference_period_start=time_period_range[0],
-                            reference_period_end=time_period_range[1],
-                        )
-                        self._session.add(conflict_event_row)
+                            fatalities = None
+                            if f"#fatality+num+{event_type}" in hxl_tags:
+                                fatalities = values[
+                                    hxl_tags.index(
+                                        f"#fatality+num+{event_type}"
+                                    )
+                                ][admin_code][irow]
+                            month = values[
+                                hxl_tags.index(f"#date+month+{event_type}")
+                            ][admin_code][irow]
+                            year = values[
+                                hxl_tags.index(f"#date+year+{event_type}")
+                            ][admin_code][irow]
+                            time_period_range = parse_date_range(
+                                f"{month} {year}", "%B %Y"
+                            )
+                            row = (
+                                resource_id,
+                                admin2_code,
+                                event_type,
+                                events,
+                                fatalities,
+                                time_period_range[0],
+                                time_period_range[1],
+                            )
+                            if row in rows:
+                                number_duplicates += 1
+                                continue
+                            rows.append(row)
+                            conflict_event_row = DBConflictEvent(
+                                resource_hdx_id=resource_id,
+                                admin2_ref=self._admins.admin2_data[
+                                    admin2_code
+                                ],
+                                event_type=event_type,
+                                events=events,
+                                fatalities=fatalities,
+                                reference_period_start=time_period_range[0],
+                                reference_period_end=time_period_range[1],
+                            )
+                            self._session.add(conflict_event_row)
+                    self._session.commit()
             if number_duplicates > 0:
                 add_message(
                     errors, dataset_name, f"{number_duplicates} duplicate rows"
                 )
 
-        self._session.commit()
         for error in sorted(errors):
             logger.error(error)
